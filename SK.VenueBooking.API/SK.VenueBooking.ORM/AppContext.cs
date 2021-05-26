1f09e9d262b64581052ca2ddc897c6794d823a30
sathishgks <sathishgks@gmail.com>
2021-05-26 12:34:08 +0530

    Dapper Changes

---
+++ SK.VenueBooking.API/SK.VenueBooking.ORM/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK.VenueBooking.ORM
+{
+   public class AppContext:DbContext
+    {
+        public AppContext() { }
+        public AppContext(DbContextOptions<AppContext> options) : base(options) { }
+    }
+}
--- SK.VenueBooking.API/SK.VenueBooking.ORM/IDatabase.cs
+++ SK.VenueBooking.API/SK.VenueBooking.ORM/IDatabase.cs
@@ -1,12 +1,20 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace SK.VenueBooking.ORM
 {
-    interface IDatabase
+    interface IDatabase : IDisposable
     {
+        DbConnection GetDbconnection();
+        T Get<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        List<T> GetAll<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        int Execute(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        T Insert<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
+        T Update<T>(string sp, DynamicParameters parms, CommandType commandType = CommandType.StoredProcedure);
     }
 }
